09f6d3f9d8821d25de6e835263e697945f328938
artofmemory <a01040123903@gmail.com>
2016-08-24 15:56:31 +0900

    modified2

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth.views import login, logout
 from .views import signup
 from django.views.generic import TemplateView
-fr
+
 
 urlpatterns = [
     # Examples:
