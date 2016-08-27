ba75333edd4fd05c7de5b0cd4299520cc1a751c0
artofmemory <a01040123903@gmail.com>
2016-08-27 20:52:05 +0900

    again

--- Source/urls.py
+++ Source/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^', views.post_list, name='post_list'),
+    url(r'$', views.post_list, name='post_list'),
 	url(r'^new/$', views.post_new, name='post_new'),
     url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
